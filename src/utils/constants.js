0f27f948e5a128a331a5933f9d5e3dd8521422dd
FerNyXnX <fernyxnx@gmail.com>
2025-10-22 22:22:04 -0700

    Feat(utils): Create app constants
    - Define API_URL: https://api.pixyn.com
    - Create CONTACT_INFO object
      - email: pixynstudio@gmail.com
      - phone: +52 123 456 7890
      - whatsapp: +521234567890
    - Export constants for global usage
    - Centralize configuration values

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const API_URL = 'https://api.pixyn.com';
+
+export const CONTACT_INFO = {
+  email: 'pixynstudio@gmail.com',
+  phone: '+52 123 456 7890',
+  whatsapp: '+521234567890',
+};
+
+export default {
+  API_URL,
+  CONTACT_INFO,
+};
